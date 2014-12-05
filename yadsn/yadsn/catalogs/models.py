8673ae3d9ec9629aeb05a0d26470ec9df6e49cf0
Roman Mogilatov <roman.mogilatov@cupid.com>
2014-12-05 09:52:46 +0200

    updating yadsn catalog

--- yadsn/yadsn/catalogs/models.py
+++ yadsn/yadsn/catalogs/models.py
@@ -16,17 +16,23 @@ class ModelsCatalog(Catalog):
     @provides('user_manager')
     def provide_user_manager(self):
         """
-        :rtype: users.models.UserManager
+        :rtype: users.models.user.UserManager
         """
         return users.models.user.UserManager()
 
     @provides('subscription_manager')
     def provide_subscription_manager(self):
+        """
+        :rtype: users.models.user.SubscriptionManager
+        """
         return users.models.user.SubscriptionManager()
 
     @provides('auth_manager')
     def provide_auth_manager(self):
-        return users.models.auth.AuthManager
+        """
+        :rtype: users.models.auth.AuthManager
+        """
+        return users.models.auth.AuthManager()
 
 
 catalog = ModelsCatalog()
