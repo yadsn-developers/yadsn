0ba01a9ca532d1ebc778934f8cfb3fa7284ec8a6
Roman Mogilatov <roman.mogilatov@cupid.com>
2015-01-01 23:56:09 +0200

    removing new year view

--- yadsn/frontend/website/urls.py
+++ yadsn/frontend/website/urls.py
@@ -1,12 +1,10 @@
 from django.conf.urls import url
 
 from .views.landing import Landing
-from .views.happy_new_year import HappyNewYear
 from .views.login import Login
 
 
 urlpatterns = [
     url(r'^$', Landing.as_view(), name='landing'),
-    url(r'^happy_new_year/$', HappyNewYear.as_view(), name='happy_new_year'),
     url(r'^login/$', Login.as_view(), name='login'),
 ]
